96107fb054503b684c64541247d7791bd8afca82
Hasan Muslemani <hasan_muslemani@hotmail.com>
2022-07-23 22:12:31 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './component/Home';
+import Navbar from './component/Navbar';
+import {Routes, Route} from 'react-router-dom';
+import Products from './component/Products';
+import Product from './component/Product';
+import Login from './component/Login';
+import Cart from './component/Cart';
+import Logout from './component/Logout';
+import Register from './component/Register';
+import About from './component/About';
+import Contact from './component/Contact';
+import Checkout from './component/Checkout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/products" element={<Products/>} />
+        <Route path="/products/:id" element={<Product/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/logout" element={<Logout/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/checkout" element={<Checkout/>} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/component/Logout.jsx
@@ -0,0 +1,71 @@
+import React, {useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
+import eventBus from '../EventBus';
+
+const Logout = () => {
+
+    const navigate = useNavigate();
+    let componentMounted = true;
+
+    useEffect(() => {
+        if(localStorage.getItem("token") === null) {
+            navigate("/login");
+            return;
+        }
+        const logout = async () => {
+            let response = await fetch("http://127.0.0.1:8000/api/logout", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization" : "Bearer " + localStorage.getItem("token")
+            }
+        });
+            if (componentMounted) {
+                if(response.status >= 200 && response.status <= 299) {
+                    localStorage.removeItem("token");
+                    eventBus.dispatch("logout", null);
+                    navigate('/');
+                }
+                else {
+                    navigate('/');
+                }
+            }
+
+            return () => {
+                componentMounted = false;
+            }
+        }
+
+        logout();
+    }, []);
+
+    const logout1 = async () => {
+        let response = await fetch("http://127.0.0.1:8000/api/logout", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization" : "Bearer " + localStorage.getItem("token")
+            }
+        });
+        if(200 >= response.status <= 299) {
+            localStorage.removeItem("token");
+            eventBus.dispatch("logout", null);
+            navigate('/');
+        }
+        else {
+            navigate('/');
+        }
+    }
+
+    return (
+        <div className="d-flex justify-content-center align-items-center" >
+            <div className="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+    );
+}
+
+export default Logout;
