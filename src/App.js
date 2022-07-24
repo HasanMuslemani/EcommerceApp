ffeb8881c5bf12189207a9a645bfd5c4d50d60e1
Hasan Muslemani <hasan_muslemani@hotmail.com>
2022-07-23 23:19:56 -0400

    Update gh-pages

--- src/App.js
+++ src/App.js
@@ -17,16 +17,16 @@ function App() {
     <>
       <Navbar/>
       <Routes>
-        <Route path="" element={<Home/>} />
-        <Route path="products" element={<Products/>} />
-        <Route path="products/:id" element={<Product/>} />
-        <Route path="about" element={<About/>} />
-        <Route path="contact" element={<Contact/>} />
-        <Route path="login" element={<Login/>} />
-        <Route path="register" element={<Register/>} />
-        <Route path="logout" element={<Logout/>} />
-        <Route path="cart" element={<Cart/>} />
-        <Route path="checkout" element={<Checkout/>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/products" element={<Products/>} />
+        <Route path="/products/:id" element={<Product/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/logout" element={<Logout/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/checkout" element={<Checkout/>} />
       </Routes>
     </>
   );
